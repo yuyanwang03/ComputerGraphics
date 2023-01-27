e31c9c18011fc782a5eb13dce54bd567798ce5c8
wangyuyan03 <yuyan.wang01@estudiant.upf.edu>
2023-01-27 16:04:37 +0100

    app framebuffer resize

--- src/framework/entity.h
+++ src/framework/entity.h
@@ -2,9 +2,27 @@
 #ifndef entity_h // Or put #pragma once
 #define entity_h
 
-class Entity {
-    // Matrix
-    // Mesh
+#include "main/includes.h"
+#include "framework.h"
+#include "mesh.h"
+
+class Entity
+{
+public:
+    Matrix44 modelMatrix;
+    Mesh entityMesh;
+    
+    // Constructors
+    Entity();
+    Entity(Matrix44 matx, Mesh msh);
+    Entity(const Entity& e);
+    Entity& operator = (const Entity& c); // Assign operator
+
+    // Destructor
+    ~Entity();
+
+    void Render();
+    
 };
 
 #endif /* entity_h */
