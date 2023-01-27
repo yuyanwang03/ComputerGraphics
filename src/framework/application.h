47ad34af74989b75d7e7facdfd622c8cc0bef0e7
wangyuyan03 <yuyan.wang01@estudiant.upf.edu>
2023-01-27 16:54:21 +0100

    u

--- src/framework/application.h
+++ src/framework/application.h
@@ -71,62 +71,6 @@ public:
 	// CPU Global framebuffer
 	Image framebuffer;
     
-    struct Particle{
-        Vector2 position;
-        Vector2 direction;
-        int size;
-        float velocity;
-        Color color;
-    };
-    
-    class ParticleSystem{
-        int numParticles;
-        bool dirRight;
-        Particle* particles;
-        Application* app;
-        public:
-        ParticleSystem(Application* a) {app = a;}
-        void Init(int n) {
-            numParticles = n; particles = new Particle[numParticles]; dirRight = true;
-            for (int i=0; i<numParticles; i++){
-                particles[i].position.set(rand()%app->window_width, rand()%app->window_height);
-                particles[i].direction = Vector2(1, 0);
-                particles[i].color.Random();
-                particles[i].size = 1 + rand()%6;
-                particles[i].velocity = 30 + rand()%60;
-            }
-            return;
-        }
-        void Update(float t) {
-            for (int i=0; i<numParticles; i++){
-                particles[i].position.x += particles[i].direction.x * particles[i].velocity * t;
-                if (dirRight) {
-                    if (particles[i].position.x > app->window_width || particles[i].position.y > app->window_height) {particles[i].position.set(1, rand()%app->window_height);}
-                } else{
-                    if (particles[i].position.x < 0 || particles[i].position.y > app->window_height) {particles[i].position.set(app->window_width-1, rand()%app->window_height);}
-                }
-            }
-            this->Render();
-            return;
-        }
-        void Render(){
-            app->framebuffer.Fill(Color::BLACK);
-            for (int i=0; i<numParticles; i++){
-                app->framebuffer.DrawCircle(particles[i].position.x, particles[i].position.y, particles[i].size, particles[i].color, true);
-            }
-            return;
-        }
-        void ChangeDirection(bool right){
-            if (dirRight==right){return;}
-            for (int i=0; i<numParticles; i++){
-                if (right) {particles[i].direction.x = 1; dirRight=true;}
-                else {particles[i].direction.x = -1; dirRight = false;}
-            }
-        }
-    };
-    
-    ParticleSystem animation;
-    
 	// Constructor and main methods
 	Application(const char* caption, int width, int height);
 	~Application();
