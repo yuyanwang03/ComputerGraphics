eab50c2713f20699ca12ec4135e66801a0b1440c
wangyuyan03 <yuyan.wang01@estudiant.upf.edu>
2023-01-27 15:18:04 +0100

    add entity class

---
+++ src/framework/entity.cpp
@@ -0,0 +1,8 @@
+//
+//  entity.cpp
+//  ComputerGraphics
+//
+//  Created by Yuyan Wang on 27/1/23.
+//
+
+#include "entity.h"
---
+++ src/framework/entity.h
@@ -0,0 +1,12 @@
+//
+//  entity.h
+//  ComputerGraphics
+//
+//  Created by Yuyan Wang on 27/1/23.
+//
+
+#ifndef entity_h
+#define entity_h
+
+
+#endif /* entity_h */
